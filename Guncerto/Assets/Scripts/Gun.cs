aaeff39fc66f4b2d68cdf36daf0b6455b4170a11
emirgiray <emiryondemli@gmail.com>
2022-12-22 17:50:17 +0300

    Reload, recoil and muzzle flash

--- Guncerto/Assets/Scripts/Gun.cs
+++ Guncerto/Assets/Scripts/Gun.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Gun : MonoBehaviour
 {
-    public TMP_Text AmmoText;
+    public Vector3 upRecoil;
+    private Vector3 originalRot;
+    public bool isRecoilOn = false;
+    public VisualEffect muzzleFlashVFX;
+    public Text AmmoText;
     public ActionBasedController controller;
 
     public GameObject Muzzle;
@@ -19,10 +25,11 @@ public class Gun : MonoBehaviour
     public float fireCoolDown; //Lower values mean faster shooting
     private float nextTimeToShoot = 0f;
 
-   
+
     // Start is called before the first frame update
     void Start()
     {
+        originalRot = transform.localEulerAngles;
         currentAmmo = maxAmmo;
         AmmoText.text = currentAmmo.ToString();
         nextTimeToShoot = 20f;
@@ -31,37 +38,56 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isRecoilOn)
+        {
+            AddRecoil();
+            isRecoilOn = false;
+        }
+        else
+        {
+            StopRecoil();
+        }
         nextTimeToShoot += Time.deltaTime;
-        controller.activateAction.action.performed += Action_performed;//This gets the input from assigned controller(press Alt and Enter after +=)
+        controller.activateAction.action.performed += Action_performed;//This gets the input from assigned controller(press something after += to auto complete)
+        controller.selectAction.action.performed += Action_performed2;
     }
 
     private void Action_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
         if (nextTimeToShoot >= fireCoolDown)
         {
-            if (currentAmmo <= maxAmmo && currentAmmo > 0)
+            if (currentAmmo <= maxAmmo && currentAmmo > 0 && isReloading == false)
             {
                 Shoot();
                 nextTimeToShoot = 0f;
                 //decrease ammo counter on screen here
             }
-            
-        }
-        
-    }
 
+        }
+
+    }
+    private void Action_performed2(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        if (currentAmmo < maxAmmo)
+        {
+            StartCoroutine(Reload());
+        }
+
+    }
     void Shoot()
     {
+        isRecoilOn = true;
+        muzzleFlashVFX.Play(); //Play muzzle flash vfx
         currentAmmo--; //Decrease current ammo
         AmmoText.text = currentAmmo.ToString();
         RaycastHit hit;
         if (Physics.Raycast(Muzzle.transform.position, Muzzle.transform.forward, out hit))//Send a hit from Muzzle 
         {
-            if (hit.transform.GetComponent<BoxCollider>())
+            if (hit.transform.GetComponent<BoxCollider>()) //This can be changed later or can add more colliders for more points
             {
                 Debug.Log(hit.transform.name);
             }
-            
+
         }
     }
     IEnumerator Reload()
@@ -69,6 +95,15 @@ public class Gun : MonoBehaviour
         isReloading = true;
         yield return new WaitForSeconds(reloadTime);
         currentAmmo = maxAmmo;
+        AmmoText.text = currentAmmo.ToString();
         isReloading = false;
+    }
+    void AddRecoil()
+    {
+        transform.localEulerAngles += upRecoil;
+    }
+    void StopRecoil()
+    {
+        transform.localEulerAngles = originalRot;
     }
 }
